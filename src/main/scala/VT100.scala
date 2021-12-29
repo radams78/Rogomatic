29738d6cc0262f10b839f8682aabb9863c618e85
Robin Adams <robin.adams.78@gmail.com>
2021-12-28 21:12:44 -0600

    Extract screen variable

--- src/main/scala/VT100.scala
+++ src/main/scala/VT100.scala
@@ -1,6 +1,7 @@
 class VT100 {
-  var firstChar = ' '
-  def getScreen() : Seq[String] = Seq(firstChar + " " * 79) ++ Seq.fill(23)(" " * 80)
+  var screen : Array[Array[Char]] = Array.fill(24)(Array.fill(80)(' '))
+  
+  def getScreen() : Seq[String] = screen.map(_.mkString)
 
-  def sendChar(char : Char) : Unit = firstChar = char
+  def sendChar(char : Char) : Unit = screen(0)(0) = char
 }
