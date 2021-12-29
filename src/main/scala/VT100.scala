f613ebfa29bf73983a9e50c32eaa69c60d7fe016
Robin Adams <robin.adams.78@gmail.com>
2021-12-28 21:16:05 -0600

    Extract cursor

--- src/main/scala/VT100.scala
+++ src/main/scala/VT100.scala
@@ -1,7 +1,10 @@
 class VT100 {
   private var screen : Array[Array[Char]] = Array.fill(24)(Array.fill(80)(' '))
+  private var cursor : Cursor = Cursor(1, 1)
   
   def getScreen() : Seq[String] = screen.map(_.mkString)
 
-  def sendChar(char : Char) : Unit = screen(0)(0) = char
+  def sendChar(char : Char) : Unit = screen(cursor.y - 1)(cursor.x - 1) = char
+
+  private class Cursor(val x : Int, val y : Int)
 }
