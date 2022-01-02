22144967607b62693019d6473b400b26b037edc9
Robin Adams <robin.adams.78@gmail.com>
2022-01-01 18:49:53 -0600

    Factory method for VT100

--- src/main/scala/VT100.scala
+++ src/main/scala/VT100.scala
@@ -2,10 +2,7 @@ import scala.collection.immutable.Queue
 import scala.collection.mutable.Stack
 import java.{util => ju}
 
-class VT100(x : Int = 1, y : Int = 1, screenContents : String = "") {
-  private val display : VT100Display = VT100Display(x, y, screenContents)
-  private val inputBuffer : InputBuffer = InputBuffer(display = display)
-  
+class VT100(display : VT100Display, inputBuffer : InputBuffer) {
   def getScreen() : Seq[String] = display.getScreen()
 
   def getCursorX() : Int = display.getCursorX()
@@ -24,4 +21,9 @@ object VT100 {
   val CR = '\u000d'
   val ESC = '\u001b'
   val DEL = '\u007f'
+
+  def apply(x : Int = 1, y : Int = 1, screenContents : String = "") = {
+    val display = VT100Display(x, y, screenContents)
+    new VT100(display, InputBuffer(display))
+  }
 }
