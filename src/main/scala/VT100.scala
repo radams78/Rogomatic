ac19acff4a4f4e106ef8c9d9b772c76feec1c484
Robin Adams <robin.adams.78@gmail.com>
2022-01-01 18:01:46 -0600

    Finish extracting VT100Display

--- src/main/scala/VT100.scala
+++ src/main/scala/VT100.scala
@@ -5,14 +5,12 @@ import java.{util => ju}
 class VT100(x : Int = 1, y : Int = 1, screenContents : String = ""):
   private var display : VT100Display = VT100Display(x, y, screenContents)
   private var inputBuffer : InputBuffer = InputBuffer()
-  private var cursor : VT100.Cursor = VT100.Cursor(x, y)
-  private var controlSeq = 0
   
   def getScreen() : Seq[String] = display.getScreen()
 
-  def getCursorX() : Int = cursor.x
+  def getCursorX() : Int = display.getCursorX()
 
-  def getCursorY() : Int = cursor.y
+  def getCursorY() : Int = display.getCursorY()
 
   def sendChar(char : Char) : Unit = 
     char match 
@@ -39,11 +37,11 @@ class VT100(x : Int = 1, y : Int = 1, screenContents : String = ""):
       case None => None
 
   private def performAction(charSeq : CharSeq) : Unit = charSeq match
-    case CharSeq.Backspace => backspace()
-    case CharSeq.Linefeed => lineFeed()
-    case CharSeq.CarriageReturn => cursor = VT100.Cursor(1, cursor.y)
-    case CharSeq.CursorBackwards(n) => for i <- 1 to n do backspace()
-    case CharSeq.NormalChar(c) => printChar(c)
+    case CharSeq.Backspace => display.backspace()
+    case CharSeq.Linefeed => display.lineFeed()
+    case CharSeq.CarriageReturn => display.carriageReturn()
+    case CharSeq.CursorBackwards(n) => for i <- 1 to n do display.backspace()
+    case CharSeq.NormalChar(c) => display.printChar(c)
 
   private enum CharSeq:
     case Backspace
@@ -51,23 +49,6 @@ class VT100(x : Int = 1, y : Int = 1, screenContents : String = ""):
     case CarriageReturn
     case CursorBackwards(n : Int)
     case NormalChar(c : Char)
-
-  private def printChar(char : Char) = 
-    display.printChar(char, cursor)
-    advanceCursor()
-
-  private def backspace() : Unit =
-    if (cursor.x > 1) cursor = VT100.Cursor(cursor.x - 1, cursor.y)
-
-  private def advanceCursor() : Unit =
-    if cursor.x < 80 then cursor = VT100.Cursor(cursor.x + 1, cursor.y)
-
-  private def lineFeed() : Unit =
-    if cursor.y == 24
-    then scroll()
-    else cursor = VT100.Cursor(cursor.x, cursor.y + 1)
-
-  private def scroll() : Unit = display.scroll()
 
 object VT100:
   val NUL = '\u0000'
@@ -78,9 +59,3 @@ object VT100:
   val CR = '\u000d'
   val ESC = '\u001b'
   val DEL = '\u007f'
-
-  case class Cursor(x : Int, y : Int):
-    assert(x > 0, s"Cursor moved off left edge of screen: ($x,$y)")
-    assert(x <= 80, s"Cursor moved off right edge of screen: ($x,$y)")
-    assert(y > 0, s"Cursor moved off top of screen: ($x,$y)")
-    assert(y <= 24, s"Cursor moved off bottom of screen: ($x,$y)")
--- src/main/scala/VT100Display.scala
+++ src/main/scala/VT100Display.scala
@@ -1,15 +1,44 @@
 class VT100Display(x : Int, y : Int, screenContents : String):
+  private var cursor : VT100Display.Cursor = VT100Display.Cursor(x, y)
+
   private var screen : Array[Array[Char]] = 
     screenContents
       .split("\n")
-      .padTo(24,"")
-      .map(_.padTo(80,' ').toCharArray)
+      .padTo(VT100Display.HEIGHT,"")
+      .map(_.padTo(VT100Display.WIDTH,' ').toCharArray)
 
   def getScreen() : Seq[String] = screen.map(_.mkString)
 
-  def printChar(char : Char, cursor : VT100.Cursor) : Unit = screen(cursor.y - 1)(cursor.x - 1) = char
+  def getCursorX() : Int = cursor.x
 
-  def scroll() : Unit =
-    for y <- 0 to 22 do screen(y) = screen(y+1)
-    screen(23) = Array.fill(80)(' ')
+  def getCursorY() : Int = cursor.y
 
+  def printChar(char : Char) : Unit = 
+      screen(cursor.y - 1)(cursor.x - 1) = char
+      advanceCursor()
+
+  def backspace() : Unit = if (cursor.x > 1) cursor = VT100Display.Cursor(cursor.x - 1, cursor.y)
+
+  def carriageReturn() : Unit = cursor = VT100Display.Cursor(1, cursor.y)
+
+  def lineFeed() : Unit =
+    if cursor.y == VT100Display.HEIGHT
+    then scroll()
+    else cursor = VT100Display.Cursor(cursor.x, cursor.y + 1)
+
+  private def advanceCursor() : Unit =
+    if cursor.x < VT100Display.WIDTH then cursor = VT100Display.Cursor(cursor.x + 1, cursor.y)
+
+  private def scroll() : Unit =
+    for y <- 0 until VT100Display.HEIGHT - 1 do screen(y) = screen(y+1)
+    screen(VT100Display.HEIGHT - 1) = Array.fill(VT100Display.WIDTH)(' ')
+
+object VT100Display:
+  private val WIDTH = 80
+  private val HEIGHT = 24
+
+  private case class Cursor(x : Int, y : Int):
+    assert(x > 0, s"Cursor moved off left edge of screen: ($x,$y)")
+    assert(x <= WIDTH, s"Cursor moved off right edge of screen: ($x,$y)")
+    assert(y > 0, s"Cursor moved off top of screen: ($x,$y)")
+    assert(y <= HEIGHT, s"Cursor moved off bottom of screen: ($x,$y)")
