b00e41294c667b7313f4e48f73b4a9ef0a6bfaf6
Robin Adams <robin.adams.78@gmail.com>
2022-01-02 19:18:41 -0600

    Move logic into Interpreter

--- src/main/scala/vt100/Interpreter.scala
+++ src/main/scala/vt100/Interpreter.scala
@@ -1,24 +1,25 @@
 package vt100
 
 class Interpreter(display : VT100Display) {
-    def backspace() : Unit = if display.getCursorX() != 1 then display.moveCursorLeft()
+    def backspace() : Unit = 
+        display.setCursorX((display.getCursorX() - 1).max(1))
 
     def lineFeed() : Unit = 
         if display.getCursorY() == VT100Display.HEIGHT
         then display.scroll()
-        else display.moveCursorDown()
+        else display.setCursorY(display.getCursorY() + 1)
   
     def carriageReturn() : Unit = display.setCursorX(1)
 
     def cursorBackwards(n : Int) : Unit = {
         assert(n >= 0)
         val distance = if n == 0 then 1 else n
-        if (display.getCursorX() - n < 0) display.setCursorX(1) else display.moveCursorLeft(distance)
+        display.setCursorX((display.getCursorX() - distance).max(1))
     }
 
     def cursorDown(n : Int) : Unit = {
         assert(n >= 0)
         val distance = if n == 0 then 1 else n
-        display.setCursorY((display.getCursorY()+distance).max(VT100Display.HEIGHT))
+        display.setCursorY((display.getCursorY() + distance).max(VT100Display.HEIGHT))
     }
 }
