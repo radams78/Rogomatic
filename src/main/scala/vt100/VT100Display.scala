46e6530ea73dab00dd3f706a0fc4223797269f25
Robin Adams <robin.adams.78@gmail.com>
2022-01-02 22:51:37 -0600

    Remove cursorPosition

--- src/main/scala/vt100/VT100Display.scala
+++ src/main/scala/vt100/VT100Display.scala
@@ -23,8 +23,6 @@ private class VT100Display(private val x : Int,
 
   def setCursorY(y : Int) : Unit = cursor = VT100Display.Cursor(cursor.x, y)
 
-  def cursorPosition(x : Int, y : Int) : Unit = cursor = VT100Display.Cursor(x, y) 
-
   private def advanceCursor() : Unit =
     if cursor.x < VT100Display.WIDTH then cursor = VT100Display.Cursor(cursor.x + 1, cursor.y)
 
