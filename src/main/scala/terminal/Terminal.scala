cd0bb21dccbba52eed5f52416203d395f67f2d27
Robin Adams <robin.adams.78@gmail.com>
2022-01-04 15:10:45 -0600

    Extract screenContents variable

--- src/main/scala/terminal/Terminal.scala
+++ src/main/scala/terminal/Terminal.scala
@@ -1,17 +1,14 @@
 package terminal
 
 class Terminal {
-  var firstChar = ' '
+  private var screenContents : Array[Array[Char]] = Array.fill(24, 80)(' ')
   var cursorX = 1
 
-  def getScreen() : Seq[String] = Seq(
-      firstChar + " " * 79
-    ) ++ Seq.fill(23)(" " * 80)
-
+  def getScreen() : Seq[String] = screenContents.map(_.mkString).toSeq
   def getCursorX() : Int = cursorX
   def getCursorY() : Int = 1
   def sendChar(char : Char) : Unit = {
-    firstChar = char
+    screenContents(0)(0) = char
     cursorX = 2
   }
 }
