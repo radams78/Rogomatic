85b2d3aa8e8b9b86825595183e9451a55b123b6d
Robin Adams <robin.adams.78@gmail.com>
2022-01-04 15:25:09 -0600

    Extract NUL

--- src/main/scala/terminal/Terminal.scala
+++ src/main/scala/terminal/Terminal.scala
@@ -1,9 +1,7 @@
 package terminal
 
 class Terminal {
-  private val HEIGHT = 24
-  private val WIDTH = 80
-  private var screenContents : Array[Array[Char]] = Array.fill(HEIGHT, WIDTH)(' ')
+  private var screenContents : Array[Array[Char]] = Array.fill(Terminal.HEIGHT, Terminal.WIDTH)(' ')
   private var cursorX = 1
   private var cursorY = 1
 
@@ -12,11 +10,17 @@ class Terminal {
   def getCursorY() : Int = cursorY
 
   def sendChar(char : Char) : Unit = char match {
-      case '\u0000' => ()
+      case Terminal.NUL => ()
       case c if ! c.isControl => {
         screenContents(cursorY - 1)(cursorX - 1) = char
-        if (cursorX < WIDTH) then cursorX += 1
+        if (cursorX < Terminal.WIDTH) then cursorX += 1
       }
       case c => throw new Error("Unrecognized character: " + c)
   }
+}
+
+object Terminal {
+  private val HEIGHT = 24
+  private val WIDTH = 80
+  private val NUL = '\u0000'
 }
