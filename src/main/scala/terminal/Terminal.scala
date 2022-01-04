8ff0df6770b80a0f2043941bba9189027a8bb5ee
Robin Adams <robin.adams.78@gmail.com>
2022-01-04 15:12:16 -0600

    Extract cursorY variable

--- src/main/scala/terminal/Terminal.scala
+++ src/main/scala/terminal/Terminal.scala
@@ -3,12 +3,13 @@ package terminal
 class Terminal {
   private var screenContents : Array[Array[Char]] = Array.fill(24, 80)(' ')
   private var cursorX = 1
+  private var cursorY = 1
 
   def getScreen() : Seq[String] = screenContents.map(_.mkString).toSeq
   def getCursorX() : Int = cursorX
-  def getCursorY() : Int = 1
+  def getCursorY() : Int = cursorY
   def sendChar(char : Char) : Unit = {
-    screenContents(0)(0) = char
-    cursorX = 2
+    screenContents(cursorY - 1)(cursorX - 1) = char
+    cursorX += 1
   }
 }
