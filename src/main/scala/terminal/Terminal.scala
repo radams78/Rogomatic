22dd5c6f3ef6aeebdf969d8be1fc6e30edf191d6
Robin Adams <robin.adams.78@gmail.com>
2022-01-04 21:38:54 -0600

    Extract parseSequenceAfterCSI

--- src/main/scala/terminal/Terminal.scala
+++ src/main/scala/terminal/Terminal.scala
@@ -44,13 +44,15 @@ class Terminal(x : Int = 1, y : Int = 1) {
   }
 
   private def parseSequenceAfterEscape(tail : Queue[Char]) : Unit = tail.dequeueOption match
-    case Some('[', tail) => tail.dequeueOption match
-      case Some('D', tail) =>
-        if cursorX > 1 then cursorX -= 1
-        inputBuffer = tail
-      case Some(c, tail) => throw new Error("Unrecognized escape sequence: ESC + [ + " + c)
-      case None => ()
+    case Some('[', tail) => parseSequenceAfterCSI(tail)
     case Some(c, tail) => throw new Error("Unrecognized escape sequence: ESC + " + c)
+    case None => ()
+
+  private def parseSequenceAfterCSI(tail : Queue[Char]) : Unit =  tail.dequeueOption match
+    case Some('D', tail) =>
+      if cursorX > 1 then cursorX -= 1
+      inputBuffer = tail
+    case Some(c, tail) => throw new Error("Unrecognized escape sequence: ESC + [ + " + c)
     case None => ()
 }
 
