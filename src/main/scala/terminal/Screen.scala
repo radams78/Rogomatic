4d98f3754a86b70e7fd401499b486e74e16193ef
Robin Adams <robin.adams.78@gmail.com>
2022-01-06 07:22:33 -0600

    Rename screenContents to contents

--- src/main/scala/terminal/Screen.scala
+++ src/main/scala/terminal/Screen.scala
@@ -1,21 +1,21 @@
 package terminal
 
 class Screen(initialContents : String) {
-  var screenContents : Array[Array[Char]] = 
+  var contents : Array[Array[Char]] = 
     initialContents
     .split('\n')
     .padTo(24, "")
     .map(_.padTo(80,' ').toCharArray)
     .toArray
 
-  def getContents() : Seq[String] = screenContents.map(_.mkString).toSeq
+  def getContents() : Seq[String] = contents.map(_.mkString).toSeq
 
   def printChar(x : Int, y : Int, char : Char) : Unit = {
       assert(1 <= x)
       assert(x <= Terminal.WIDTH)
       assert(1 <= y)
       assert(y <= Terminal.HEIGHT)
-      screenContents(y - 1)(x - 1) = char
+      contents(y - 1)(x - 1) = char
     }
 
   def eraseLine(y : Int) : Unit = {
@@ -49,5 +49,5 @@ class Screen(initialContents : String) {
     eraseFromStartOfLine(x, y)
   }
 
-  def eraseScreen() : Unit = screenContents = Array.fill(24)(Array.fill(80)(' '))
+  def eraseScreen() : Unit = contents = Array.fill(24)(Array.fill(80)(' '))
 }
