eb7b50831811e2cae090382f3c42f8fd8511c045
Robin Adams <robin.adams.78@gmail.com>
2022-01-11 20:37:56 +0100

    Use Rogue class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,24 +7,6 @@ import scala.concurrent.Future
 import concurrent.ExecutionContext.Implicits.global
 
 @main def rgm : Unit = {
-  val cmd = Array("/usr/games/rogue")
-  val env = Map("TERM" -> "vt100")
-  val process = new PtyProcessBuilder()
-    .setCommand(cmd)
-    .setEnvironment(env)
-    .start()
-
-  val os = process.getOutputStream
-  val is = process.getInputStream
-
-  val terminal = Terminal()
-  Future{
-    while(true) {
-      terminal.receiveChar(is.read.toChar)
-    }
-  }
-  Thread.sleep(1000)
-  println(terminal.getScreen().mkString("\n"))
-
-  process.destroy()
+  val rogue = Rogue()
+  println(rogue.getScreen().mkString("\n"))
 }
