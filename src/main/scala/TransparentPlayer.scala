6136f1a8ecf94a1a6747bfeda8211740ddd966fe
Robin Adams <robin.adams.78@gmail.com>
2022-01-13 02:37:01 +0100

    TransparentPlayer gets a command from user

--- src/main/scala/TransparentPlayer.scala
+++ src/main/scala/TransparentPlayer.scala
@@ -1,6 +1,6 @@
 class TransparentPlayer(user : IUser, rogue : IRogue) {
     user.displayScreen(rogue.getScreen())
-    for (k <- Command.keypresses(Command.Quit)) {
+    for (k <- Command.keypresses(user.getCommand())) {
         rogue.sendKeypress(k)
     }
 }
