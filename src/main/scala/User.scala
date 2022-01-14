670c0963373eef2e8baac67c4deb259b523c6c4f
Robin Adams <robin.adams.78@gmail.com>
2022-01-14 22:21:56 +0100

    Accept input from user

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,5 +1,17 @@
+import scala.io.StdIn
+import scala.annotation.tailrec
+
 class User extends IUser {
   override def displayScreen(screen : Seq[String]) : Unit = println(screen.mkString("\n"))
 
-  override def getCommand() : Command = Command.Quit
+  override def getCommand() : Command = {
+    @tailrec
+    def getCommand0() : Command = StdIn.readChar match {
+      case 'Q' => return Command.Quit
+      case c => 
+        println(s"Unrecognised command: $c")
+        getCommand0()
+      }
+    getCommand0()
+  }
 }
