55a82179e0866c992a945470989fbd257aca11dc
Robin Adams <robin.adams.78@gmail.com>
2022-01-15 20:18:18 +0100

    Begin to refactor parser for inventory screen

--- src/main/scala/TransparentPlayer.scala
+++ src/main/scala/TransparentPlayer.scala
@@ -1,14 +1,24 @@
 class TransparentPlayer(user : IUser, rogue : IRogue) {
     user.displayScreen(rogue.getScreen())
-    user.displayInventory(
-        Inventory(Map(
+    rogue.sendKeypress('i')
+    val inventory =
+        if rogue.getScreen().slice(0,6) == Seq(
+        "                                                a) some food",
+        "                                                b) +1 ring mail [4] being worn",
+        "                                                c) a +1,+1 mace in hand",
+        "                                                d) a +1,+0 short bow",
+        "                                                e) 31 +0,+0 arrows",
+        "                                                --press space to continue--"
+        ).map(_.padTo(80,' ')) then         Inventory(Map(
           Slot('a') -> Food(1),
           Slot('b') -> RingMail(+1),
           Slot('c') -> Mace(+1,+1),
           Slot('d') -> ShortBow(+1,+0),
           Slot('e') -> Arrow(31, +0, +0)
         ))
-    )
+        else throw new Error(s"Unrecoginsed screen: \n${rogue.getScreen()}")
+    rogue.sendKeypress(' ')
+    user.displayInventory(inventory)
     for (k <- Command.keypresses(user.getCommand())) {
         rogue.sendKeypress(k)
     }
