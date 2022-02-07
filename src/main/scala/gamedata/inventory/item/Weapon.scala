423d93dd4b550c7a2348abe127fe053913fee12c
Robin Adams <robin.adams.78@gmail.com>
2022-02-07 20:38:42 +0100

    Encapsulate Bonuses

--- src/main/scala/gamedata/inventory/item/Weapon.scala
+++ src/main/scala/gamedata/inventory/item/Weapon.scala
@@ -1,35 +1,59 @@
 enum Weapon extends Wieldable {
-    case Melee(meleeType : MeleeType, bonuses: Bonuses)
-    case Thrower(throwerType : ThrowerType, bonuses: Bonuses)
-    case Missile(quantity : Int, missileType : MissileType, bonuses : Bonuses)
-}
-
-enum Bonuses {
-    case Unidentified
-    case Identified(plusToHit : Bonus, plusDamage : Bonus)
+  case Melee(meleeType: MeleeType, bonuses: Weapon.Bonuses)
+  case Thrower(throwerType: ThrowerType, bonuses: Weapon.Bonuses)
+  case Missile(quantity: Int, missileType: MissileType, bonuses: Weapon.Bonuses)
 }
 
 object Weapon {
-    def apply(quantity : Int, missileType : MissileType, plusToHit : Bonus, plusDamage : Bonus) : Weapon = 
-        new Missile(quantity, missileType, Bonuses.Identified(plusToHit, plusDamage))
+  def apply(
+      quantity: Int,
+      missileType: MissileType,
+      plusToHit: Bonus,
+      plusDamage: Bonus
+  ): Weapon =
+    new Missile(
+      quantity,
+      missileType,
+      Bonuses.Identified(plusToHit, plusDamage)
+    )
 
-    def apply(quantity : Int, missileType : MissileType, plusToHit : Int, plusDamage : Int) : Weapon = 
-        apply(quantity, missileType, Bonus(plusToHit), Bonus(plusDamage))
+  def apply(
+      quantity: Int,
+      missileType: MissileType,
+      plusToHit: Int,
+      plusDamage: Int
+  ): Weapon =
+    apply(quantity, missileType, Bonus(plusToHit), Bonus(plusDamage))
 
-    def apply(weaponType : WeaponType, plusToHit : Bonus, plusDamage : Bonus) : Weapon = weaponType match {
-        case meleeType : MeleeType => Melee(meleeType, Bonuses.Identified(plusToHit, plusDamage))
-        case throwerType : ThrowerType => Thrower(throwerType, Bonuses.Identified(plusToHit, plusDamage))
-        case missileType : MissileType => Missile(1, missileType, Bonuses.Identified(plusToHit, plusDamage))
-    }
+  def apply(
+      weaponType: WeaponType,
+      plusToHit: Bonus,
+      plusDamage: Bonus
+  ): Weapon = weaponType match {
+    case meleeType: MeleeType =>
+      Melee(meleeType, Bonuses.Identified(plusToHit, plusDamage))
+    case throwerType: ThrowerType =>
+      Thrower(throwerType, Bonuses.Identified(plusToHit, plusDamage))
+    case missileType: MissileType =>
+      Missile(1, missileType, Bonuses.Identified(plusToHit, plusDamage))
+  }
 
-    def apply(weaponType : WeaponType, plusToHit : Int, plusDamage : Int) : Weapon = 
-        apply(weaponType, Bonus(plusToHit), Bonus(plusDamage))
+  def apply(weaponType: WeaponType, plusToHit: Int, plusDamage: Int): Weapon =
+    apply(weaponType, Bonus(plusToHit), Bonus(plusDamage))
 
-    def apply(quantity : Int, missileType : MissileType) : Weapon = Missile(quantity, missileType, Bonuses.Unidentified)
+  def apply(quantity: Int, missileType: MissileType): Weapon =
+    Missile(quantity, missileType, Bonuses.Unidentified)
 
-    def apply(weaponType : WeaponType) : Weapon = weaponType match {
-        case meleeType : MeleeType => Melee(meleeType, Bonuses.Unidentified)
-        case throwerType : ThrowerType => Thrower(throwerType, Bonuses.Unidentified)
-        case missileType : MissileType => Missile(1, missileType, Bonuses.Unidentified)
-    }
+  def apply(weaponType: WeaponType): Weapon = weaponType match {
+    case meleeType: MeleeType     => Melee(meleeType, Bonuses.Unidentified)
+    case throwerType: ThrowerType => Thrower(throwerType, Bonuses.Unidentified)
+    case missileType: MissileType =>
+      Missile(1, missileType, Bonuses.Unidentified)
+  }
+
+  enum Bonuses {
+    case Unidentified
+    case Identified(plusToHit: Bonus, plusDamage: Bonus)
+  }
+
 }
