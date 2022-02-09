0cefaf57fa5701e2622dc9c6e01a82005160b040
Robin Adams <robin.adams.78@gmail.com>
2022-02-09 22:36:13 +0100

    Encapsulate subclasses of Weapon

--- src/main/scala/gamedata/inventory/item/Weapon.scala
+++ src/main/scala/gamedata/inventory/item/Weapon.scala
@@ -1,15 +1,15 @@
 trait Weapon extends Wieldable
 
 object Weapon {
-  case class Melee(meleeType: MeleeType, bonuses: Weapon.Bonuses) extends Weapon {
+  private case class Melee(meleeType: MeleeType, bonuses: Weapon.Bonuses) extends Weapon {
     override def toString = s"a $bonuses $meleeType"
   }
 
-  case class Thrower(throwerType: ThrowerType, bonuses: Weapon.Bonuses) extends Weapon {
+  private case class Thrower(throwerType: ThrowerType, bonuses: Weapon.Bonuses) extends Weapon {
     override def toString = s"a $bonuses $throwerType"
   }
 
-  case class Missile(quantity: Int, missileType: MissileType, bonuses: Weapon.Bonuses) extends Weapon {
+  private case class Missile(quantity: Int, missileType: MissileType, bonuses: Weapon.Bonuses) extends Weapon {
     override def toString = s"$quantity $bonuses " +
       s"${if (quantity > 1) missileType.plural else missileType.singular}"
   }
