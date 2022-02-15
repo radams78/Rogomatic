5f4dbeb799f9be7d482a977ed96f1f76aada32a2
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:07:46 +0100

    Every TransparentPlayer has a Mediator

--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -6,6 +6,7 @@ import rogue.IRogue
 class TransparentPlayer(user : IUser, rogue : IRogue) {
   private val player : RoguePlayer = new RoguePlayer(rogue)
   private val expert = new TransparentExpert(user)
+  private val mediator : Mediator = new Mediator()
 
   private val command = expert.getCommand(player.getScreen(), player.getInventory())
   player.performCommand(command)
