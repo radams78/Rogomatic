e6a2b48bd77d0ef595b2b5247fdd5edc2ab37037
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 07:46:20 +0100

    Tidy up code

--- src/main/scala/player/MessageLine.scala
+++ src/main/scala/player/MessageLine.scala
@@ -1,6 +1,7 @@
 package player
 
 import gamedata.Event
+
 enum MessageLine {
   case More(events: Seq[Event])
   case Message(events: Seq[Event])
--- src/main/scala/player/StatusLine.scala
+++ src/main/scala/player/StatusLine.scala
@@ -2,4 +2,15 @@ package player
 
 import gamedata.Hunger
 
-case class StatusLine(level: Int, gold: Int, hp: Int, maxhp: Int, str: Int, maxstr: Int, armor: Int, exp: Int, expLevel: Int, hunger: Hunger)
+case class StatusLine(
+    level: Int,
+    gold: Int,
+    hp: Int,
+    maxhp: Int,
+    str: Int,
+    maxstr: Int,
+    armor: Int,
+    exp: Int,
+    expLevel: Int,
+    hunger: Hunger
+)
