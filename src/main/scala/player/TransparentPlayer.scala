f124490eb89f6c8ea4221071630fc7239158bb1f
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:13:46 +0100

    Delete TransparentPlayer.expert

--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -4,8 +4,7 @@ import rogue.IRogue
 
 // TODO Keep the game going after one turn
 class TransparentPlayer(user : IUser, rogue : IRogue) {
-  private val expert = new TransparentExpert(user)
-  private val mediator : Mediator = new Mediator(new RoguePlayer(rogue), expert)
+  private val mediator : Mediator = new Mediator(new RoguePlayer(rogue), new TransparentExpert(user))
 
   private val command = mediator.expert.getCommand(mediator.player.getScreen(), mediator.player.getInventory())
   mediator.player.performCommand(command)
