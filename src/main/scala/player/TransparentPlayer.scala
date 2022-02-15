527d1dc5c313eec74d425f8da8042d377f978efb
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:15:28 +0100

    Dependency injection

--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -3,9 +3,12 @@ package player
 import rogue.IRogue
 
 // TODO Keep the game going after one turn
-class TransparentPlayer(user : IUser, rogue : IRogue) {
-  private val mediator : Mediator = new Mediator(new RoguePlayer(rogue), new TransparentExpert(user))
-
+class TransparentPlayer(user : IUser, rogue : IRogue, mediator : Mediator) {
   private val command = mediator.expert.getCommand(mediator.player.getScreen(), mediator.player.getInventory())
   mediator.player.performCommand(command)
+}
+
+object TransparentPlayer {
+  def apply(user : IUser, rogue : IRogue) : TransparentPlayer = 
+    new TransparentPlayer(user, rogue, new Mediator(new RoguePlayer(rogue), new TransparentExpert(user)))
 }
