9f19ec65b8e2f401b74e67afc4e5de057c766fcf
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:16:05 +0100

    Remove redundant fields

--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -3,12 +3,12 @@ package player
 import rogue.IRogue
 
 // TODO Keep the game going after one turn
-class TransparentPlayer(user : IUser, rogue : IRogue, mediator : Mediator) {
+class TransparentPlayer(mediator : Mediator) {
   private val command = mediator.expert.getCommand(mediator.player.getScreen(), mediator.player.getInventory())
   mediator.player.performCommand(command)
 }
 
 object TransparentPlayer {
   def apply(user : IUser, rogue : IRogue) : TransparentPlayer = 
-    new TransparentPlayer(user, rogue, new Mediator(new RoguePlayer(rogue), new TransparentExpert(user)))
+    new TransparentPlayer(new Mediator(new RoguePlayer(rogue), new TransparentExpert(user)))
 }
