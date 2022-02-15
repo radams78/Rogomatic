f1336b2cdb6a574cb68b249dc938e5dea84fe6fa
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:10:41 +0100

    Move one reference to player to Mediator

--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -8,6 +8,6 @@ class TransparentPlayer(user : IUser, rogue : IRogue) {
   private val expert = new TransparentExpert(user)
   private val mediator : Mediator = new Mediator(player)
 
-  private val command = expert.getCommand(mediator.player.getScreen(), player.getInventory())
+  private val command = expert.getCommand(mediator.player.getScreen(), mediator.player.getInventory())
   player.performCommand(command)
 }
