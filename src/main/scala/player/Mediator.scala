0f194482e05643eb56508d39cd9c3c2b6e9cfcf3
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:12:32 +0100

    Create Mediator.expert

--- src/main/scala/player/Mediator.scala
+++ src/main/scala/player/Mediator.scala
@@ -1,5 +1,5 @@
 package player
 
-class Mediator(val player : RoguePlayer) {
+class Mediator(val player : RoguePlayer, val expert : TransparentExpert) {
   
 }
--- src/main/scala/player/TransparentPlayer.scala
+++ src/main/scala/player/TransparentPlayer.scala
@@ -5,7 +5,7 @@ import rogue.IRogue
 // TODO Keep the game going after one turn
 class TransparentPlayer(user : IUser, rogue : IRogue) {
   private val expert = new TransparentExpert(user)
-  private val mediator : Mediator = new Mediator(new RoguePlayer(rogue))
+  private val mediator : Mediator = new Mediator(new RoguePlayer(rogue), expert)
 
   private val command = expert.getCommand(mediator.player.getScreen(), mediator.player.getInventory())
   mediator.player.performCommand(command)
