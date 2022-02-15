6c765c96bf38d977f2a4e35421721be191ca0d44
Robin Adams <robin.adams.78@gmail.com>
2022-02-15 08:18:46 +0100

    Factory method for Mediator

--- src/main/scala/player/Mediator.scala
+++ src/main/scala/player/Mediator.scala
@@ -1,8 +1,15 @@
 package player
 
+import rogue.IRogue
+
 class Mediator(val player : RoguePlayer, val expert : TransparentExpert) {
   def performNextCommand() : Unit = {
     val command = expert.getCommand(player.getScreen(), player.getInventory())
     player.performCommand(command)
   }
+}
+
+object Mediator {
+    def apply(rogue : IRogue, user : IUser) : Mediator =
+        new Mediator(new RoguePlayer(rogue), new TransparentExpert(user))
 }
