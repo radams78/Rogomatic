ab89b4bc49e7a75ba04eb3135fa826b1f53c344c
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:31:02 +0100

    Better toString

--- src/main/scala/gamedata/inventory/item/ArmorType.scala
+++ src/main/scala/gamedata/inventory/item/ArmorType.scala
@@ -8,4 +8,14 @@ enum ArmorType {
   case BANDEDMAIL
   case SPLINTMAIL
   case PLATEMAIL
+
+  override def toString : String = this match {
+    case LEATHER => "leather armor"
+    case RINGMAIL => "ring mail"
+    case SCALEMAIL => "scale mail"
+    case CHAINMAIL => "chain mail"
+    case BANDEDMAIL => "banded mail"
+    case SPLINTMAIL => "splint mail"
+    case PLATEMAIL => "plate mail"
+  }
 }
