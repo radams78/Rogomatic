a47e5f319aac4d23d82eee534be6db3e2adb7f08
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:04:04 +0100

    Annotate exception

--- src/main/scala/Rogomatic.scala
+++ src/main/scala/Rogomatic.scala
@@ -2,6 +2,7 @@ import player.IRoguePlayer
 import expert.IExpert
 
 class Rogomatic(val player : IRoguePlayer, val expert : IExpert) {
+  // @throws UnparsableInventoryLineException if inventory cannot be read
   def performNextCommand() : Unit = {
     val command = expert.getCommand(player.getScreen(), player.getInventory())
     player.performCommand(command)
