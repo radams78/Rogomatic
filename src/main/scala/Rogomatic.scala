4460d610a45229d2d7ac1ce1618c0691ffdadd39
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 10:08:32 +0100

    Clean up code

--- src/main/scala/Rogomatic.scala
+++ src/main/scala/Rogomatic.scala
@@ -1,21 +1,9 @@
-import rogue.IRogue
 import rogue.IRoguePlayer
-import rogue.RoguePlayerFactory
 import expert.IExpert
-import expert.transparent.IUser
-import expert.transparent.TransparentExpertFactory
 
 class Rogomatic(val player : IRoguePlayer, val expert : IExpert) {
   def performNextCommand() : Unit = {
     val command = expert.getCommand(player.getScreen(), player.getInventory())
     player.performCommand(command)
   }
-}
-
-object Rogomatic {
-    def makeTransparent(rogue : IRogue, user : IUser) : Rogomatic =
-        new Rogomatic(RoguePlayerFactory.makeRoguePlayer(rogue), TransparentExpertFactory.makeExpert(user))
-
-    def makeTransparent() : Rogomatic =
-      new Rogomatic(RoguePlayerFactory.makeRoguePlayer(), TransparentExpertFactory.makeExpert())
 }
