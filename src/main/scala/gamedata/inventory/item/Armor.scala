a624c6ff26543729af9d740f2d28ee2341cc430b
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:28:13 +0100

    Better toString

--- src/main/scala/gamedata/inventory/item/Armor.scala
+++ src/main/scala/gamedata/inventory/item/Armor.scala
@@ -1,7 +1,9 @@
 package gamedata.inventory.item
 
 // TODO Unidentified Armor
-final case class Armor(armorType : ArmorType, bonus : Bonus) extends Item
+final case class Armor(armorType : ArmorType, bonus : Bonus) extends Item {
+    override def toString : String = s"$bonus $armorType"
+}
 
 object Armor{
     def apply(armorType: ArmorType, bonus : Int) : Armor = apply(armorType, Bonus(bonus))
