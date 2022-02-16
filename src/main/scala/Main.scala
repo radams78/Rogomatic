150b89b6b5bf40b8a1dd997b80b5202d0cec622d
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 10:09:27 +0100

    Remove unnecessary dependencies

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,4 @@
-import expert.transparent.IUser
-import expert.transparent.User
-import rogue.IRogue
-import rogue.Rogue
-
 @main def rgm : Unit = {
-  val rogue : IRogue = new Rogue()
-  val user : IUser = new User()
-  val player = Rogomatic.makeTransparent(rogue, user)
+  val player = TransparentRogomaticFactory.makeTransparentRogomatic()
   player.performNextCommand()
 }
