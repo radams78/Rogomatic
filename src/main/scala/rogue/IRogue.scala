68cb41835aa813714d0c7f9381638ce5f29c1875
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:07:12 +0100

    Add todo

--- src/main/scala/rogue/IRogue.scala
+++ src/main/scala/rogue/IRogue.scala
@@ -1,7 +1,7 @@
 package rogue
 
 trait IRogue {
-    // Must return an array of 24 strings of length 80
+    // TODO Must return an array of 24 strings of length 80
   def getScreen() : Seq[String]
 
   def sendKeypress(keypress : Char) : Unit
