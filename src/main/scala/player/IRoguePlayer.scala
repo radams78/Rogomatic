93ead7d4c0f9d087bc2967c0108fad351ae6a82f
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:06:23 +0100

    Add todo about exceptions

--- src/main/scala/player/IRoguePlayer.scala
+++ src/main/scala/player/IRoguePlayer.scala
@@ -3,6 +3,7 @@ package player
 import gamedata.inventory.Inventory
 import gamedata.Command
 
+// TODO Throw UnparsableInventoryLineException in the right place
 trait IRoguePlayer {
   def getScreen() : Seq[String]
   def getInventory() : Inventory
