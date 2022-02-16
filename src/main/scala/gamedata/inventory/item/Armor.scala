c09390ee7e20e6ff1236cfa85cd1db10c5740fe9
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:10:57 +0100

    Make Armor final case class

--- src/main/scala/gamedata/inventory/item/Armor.scala
+++ src/main/scala/gamedata/inventory/item/Armor.scala
@@ -1,6 +1,7 @@
 package gamedata.inventory.item
 
-case class Armor(armorType : ArmorType, bonus : Bonus) extends Item
+// TODO Unidentified Armor
+final case class Armor(armorType : ArmorType, bonus : Bonus) extends Item
 
 object Armor{
     def apply(armorType: ArmorType, bonus : Int) : Armor = apply(armorType, Bonus(bonus))
