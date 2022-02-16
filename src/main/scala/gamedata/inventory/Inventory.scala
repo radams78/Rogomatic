1b5ff1905f9dbb0361dd0ade01bc47eb2ee92e9d
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:21:53 +0100

    Better toString

--- src/main/scala/gamedata/inventory/Inventory.scala
+++ src/main/scala/gamedata/inventory/Inventory.scala
@@ -2,4 +2,9 @@ package gamedata.inventory
 
 import item.Item
 
-final case class Inventory(items : Map[Slot, Item])
+final case class Inventory(items : Map[Slot, Item]) {
+    override def toString : String = items.toList
+        .sortWith({case ((Slot(x),_),(Slot(y),_)) => x <= y})
+        .map((slot, item) => s"$slot) $item")
+        .mkString("\n")
+}
