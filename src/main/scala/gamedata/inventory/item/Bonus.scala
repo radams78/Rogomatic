a4114ceae9d390364e89543be460a1a425072199
Robin Adams <robin.adams.78@gmail.com>
2022-02-16 19:29:33 +0100

    Better toString

--- src/main/scala/gamedata/inventory/item/Bonus.scala
+++ src/main/scala/gamedata/inventory/item/Bonus.scala
@@ -1,3 +1,5 @@
 package gamedata.inventory.item
 
-final case class Bonus(value : Int)
+final case class Bonus(value : Int) {
+    override def toString : String = (if (value >= 0) "+" else "") + value
+}
